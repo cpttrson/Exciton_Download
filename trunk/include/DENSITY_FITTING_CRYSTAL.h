3a6a4593e93e97e82a3e416303a7bf6933cfe5f8
Charles Patterson <charles.patterson@tcd.ie>
2021-06-10 18:09:58 +0100

    second commit of master

--- trunk/include/DENSITY_FITTING_CRYSTAL.h
+++
@@ -1,5 +0,0 @@
-
-void generate_coulomb_matrix_inverse_complex(ComplexMatrix*, int, FERMI*, ATOM_TRAN*, ATOM*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, SYMMETRY*, REAL_LATTICE*, REAL_LATTICE_TABLES*, RECIPROCAL_LATTICE*, JOB_PARAM*, FILES);
-
-void density_fitting_crystal_contract_integrals(int*, int*, int*, int*, int*, PAIR_TRAN*, KPOINT_TRAN*, MPI_File, Complex**, FERMI*, ATOM_TRAN*, ATOM*, SHELL*, GAUSSIAN*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, SYMMETRY*, SYMMETRY*, Q_LATTICE*, REAL_LATTICE*, REAL_LATTICE_TABLES*, RECIPROCAL_LATTICE*, JOB_PARAM*, FILES);
-
