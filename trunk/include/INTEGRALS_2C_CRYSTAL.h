6a2a7c8e18bf739bdc33e3d68b8f6d6b45933a4d
cpttrson <Charles.Patterson@tcd.ie>
2021-08-12 18:49:12 +0100

    Delete INTEGRALS_2C_CRYSTAL.h

--- trunk/include/INTEGRALS_2C_CRYSTAL.h
+++
@@ -1,4 +0,0 @@
-
-void integrals_crystal_ij(ComplexMatrix*, REAL_LATTICE*, Q_LATTICE*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, JOB_PARAM*, FILES);
-//void two_centre_coulomb1_crystal(ComplexMatrix*, REAL_LATTICE*, Q_LATTICE*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, JOB_PARAM*, FILES);
-
