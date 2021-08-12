c2e447c1908a9094358a156a26bf592d2c1f0b8c
cpttrson <Charles.Patterson@tcd.ie>
2021-08-12 18:49:24 +0100

    Delete INTEGRALS_3C_CRYSTAL.h

--- trunk/include/INTEGRALS_3C_CRYSTAL.h
+++
@@ -1,6 +0,0 @@
-
-void integrals_crystal_ija(int, TRIPLE_TRAN*, int*, Complex*, REAL_LATTICE*, Q_LATTICE*, RECIPROCAL_LATTICE*, ATOM*, SHELL*, GAUSSIAN*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, JOB_PARAM*, FILES);
-//void three_centre_coulomb1_reversed2_crystal_test1(int, TRIPLE_TRAN*, int*, Complex*, REAL_LATTICE*, Q_LATTICE*, RECIPROCAL_LATTICE*, ATOM*, SHELL*, GAUSSIAN*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, JOB_PARAM*, FILES);
-
-void three_centre_coulomb1_reversed2_crystal_test2(INTEGRAL_LIST_COMPLEX*, int, TRIPLE_TRAN*, int*, Complex*, REAL_LATTICE*, Q_LATTICE*, RECIPROCAL_LATTICE*, ATOM*, SHELL*, GAUSSIAN*, ATOM*, SHELL*, GAUSSIAN*, CRYSTAL*, JOB_PARAM*, FILES);
-
